724115b52182121150ce1459c4c56750935e80b4
kevinfs <kevinfs@me.com>
2015-04-06 16:54:06 +0200

    cleanup half_adder_utest

--- half_adder_utest.vhdl
+++ half_adder_utest.vhdl
@@ -2,26 +2,27 @@ LIBRARY IEEE;
 USE IEEE.STD_LOGIC_1164.ALL;
 
 entity half_adder_utest is
-	port (
-		RESULT, CARRY : out std_logic
-	);
 end entity half_adder_utest;
 
 architecture half_adder_utest of half_adder_utest is
 
 	signal A: std_logic := '0';
 	signal B: std_logic := '0';
-	--signal RESULT: std_logic;
-	--signal CARRY: std_logic;
+	signal RESULT: std_logic := '0';
+	signal CARRY: std_logic := '0';
 
 begin
 
+	-- INSTANCE
+
 	DUT : entity WORK.half_adder (half_adder) port map (A, B, RESULT, CARRY);
+
+	-- CASES
 
 	A <= '1' after 2 ns, '0' after 6 ns, '1' after 10 ns;
 	B <= '1' after 4 ns, '0' after 8 ns, '1' after 12 ns;
 
-
+	-- PROCESS
 
 	process(A, B)
 	begin
@@ -31,8 +32,5 @@ begin
 
 		report "The value of 'result' is " & std_logic'image(RESULT);
 	end process;
-
-
-
 
 end architecture half_adder_utest;
