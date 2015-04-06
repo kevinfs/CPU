f546a8eb05b0039d023f8db344b796379dbb9a60
kevinfs <kevinfs@me.com>
2015-04-06 14:12:12 +0200

    half_adder

---
+++ half_adder.vhdl
@@ -0,0 +1,19 @@
+LIBRARY IEEE;
+USE IEEE.STD_LOGIC_1164.ALL;
+
+entity half_adder is
+   port (
+   		A, B : in std_logic;
+    	SUM, CARRY : out std_logic
+    );
+end half_adder;
+
+architecture half_adder of half_adder is
+
+begin
+	process(A, B)
+	begin
+		SUM <= A XOR B;
+		CARRY <= A AND B;
+	end process;
+end half_adder;
