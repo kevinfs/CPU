d71751469ebda81988512c5a3098a5d04cf80c8d
kevinfs <kevinfs@me.com>
2015-04-07 18:22:13 +0200

    generic_alu + test

--- generic_alu.vhdl
+++ generic_alu.vhdl
@@ -14,34 +14,58 @@ end entity generic_alu;
 
 architecture generic_alu of generic_alu is
 
--- IMPORTS
+	-- IMPORTS
 
-	component full_adder is
+	component generic_full_adder is
+		generic(
+			SIZE : integer := 16
+		);
 		port (
-			A, B, CARRY_IN : in std_logic;
-			SUM, CARRY_OUT : out std_logic
+			A, B : in std_logic_vector(SIZE-1 downto 0);
+			CARRY_IN : in std_logic;
+			SUM : out std_logic_vector(SIZE-1 downto 0);
+			CARRY_OUT : out std_logic := '0'
 		);
 	end component;
 
--- SIGNALS
+	-- SIGNALS
 
 	signal ADD: std_logic_vector(SIZE-1 downto 0);
-	signal ADD: std_logic_vector(SIZE-1 downto 0);
+	signal SUB: std_logic_vector(SIZE-1 downto 0);
+	signal MUL: std_logic_vector(SIZE-1 downto 0);
+	signal SHI: std_logic_vector(SIZE-1 downto 0);
+	signal OVERFLOW: std_logic := '0';
+	signal UNDERFLOW: std_logic := '0';
+	signal UNDERFLOW_TMP: std_logic := '0';
+	signal NOT_B: std_logic_vector(SIZE-1 downto 0);
+	signal CPM_B: std_logic_vector(SIZE-1 downto 0);
+	signal ONE: std_logic_vector(SIZE-1 downto 0);
 
 begin
 
+	-- ROUTING
+
+	NOT_B <= (not B);
+	ONE(SIZE-1 downto 1) <= (others => '0');
+	ONE(0) <= '1';
+	UNDERFLOW <= (not UNDERFLOW_TMP);
+
+	FA_ADD : generic_full_adder generic map(SIZE) port map(A, B, '0', ADD, OVERFLOW);
+	FA_SUB : generic_full_adder generic map(SIZE) port map(A, CPM_B, '0', SUB, UNDERFLOW_TMP);
+	FA_CPM : generic_full_adder generic map(SIZE) port map(ONE, NOT_B, '0', CPM_B);
+
 	-- PROCESS
 
-	process(A, B, SEL)
+	process(ADD, SUB, MUL, SHI, SEL)
 	begin
 
-		-- use case statement
+		-- CASE ON SEL
 		case SEL is
-			when "00" =>	O <= I0;
-			when "01" =>	O <= I1;
-			when "10" =>	O <= I2;
-			when "11" =>	O <= I3;
-			when others =>	O <= "ZZZZZZZZZZZZZZZZ";
+			when "00" =>	O <= ADD;
+			when "01" =>	O <= SUB;
+			when "10" =>	O <= MUL;
+			when "11" =>	O <= SHI;
+			when others =>	O <= (others => 'Z');
 		end case;
 
 	end process;
--- generic_alu_utest.vhdl
+++ generic_alu_utest.vhdl
@@ -1,37 +1,44 @@
 LIBRARY IEEE;
 USE IEEE.STD_LOGIC_1164.ALL;
 
-entity generic_mux_10_utest is
-end entity generic_mux_10_utest;
+entity generic_alu_utest is
+end entity generic_alu_utest;
 
-architecture generic_mux_10_utest of generic_mux_10_utest is
+architecture generic_alu_utest of generic_alu_utest is
 
 	-- SIGNALS
 
-	signal I0: std_logic_vector(15 downto 0) := "0000000000000000";
-	signal I1: std_logic_vector(15 downto 0) := "1000000000000000";
-	signal I2: std_logic_vector(15 downto 0) := "1100000000000000";
-	signal I3: std_logic_vector(15 downto 0) := "1110000000000000";
-	signal I4: std_logic_vector(15 downto 0) := "1111000000000000";
-	signal I5: std_logic_vector(15 downto 0) := "1111100000000000";
-	signal I6: std_logic_vector(15 downto 0) := "1111110000000000";
-	signal I7: std_logic_vector(15 downto 0) := "1111111000000000";
-	signal I8: std_logic_vector(15 downto 0) := "1111111100000000";
-	signal I9: std_logic_vector(15 downto 0) := "1111111110000000";
-	signal SEL: std_logic_vector(3 downto 0) := "0000";
+	signal A: std_logic_vector(15 downto 0) := "0000000000000000";
+	signal B: std_logic_vector(15 downto 0) := "0000000100000000";
+	signal SEL: std_logic_vector(1 downto 0) := "00";
 	signal O: std_logic_vector(15 downto 0) := "0000000000000000";
 
 begin
 
 	-- INSTANCE
 
-	DUT : entity WORK.generic_mux_10 (generic_mux_10) port map (I0, I1, I2, I3, I4, I5, I6, I7, I8, I9, SEL, O);
+	DUT : entity WORK.generic_alu (generic_alu) port map (A, B, SEL, O);
 
 	-- CASES
 
-	SEL <= "0001" after 1 ns, "0010" after 2 ns, "0011" after 3 ns,
-			"0100" after 4 ns, "0101" after 5 ns, "0110" after 6 ns,
-			"0111" after 7 ns, "1000" after 8 ns, "1001" after 9 ns,
-			"1010" after 10 ns, "1101" after 11 ns;
+	SEL <= "01" after 10 ns, "10" after 20 ns, "11" after 30 ns;
+	A <= "1001000100000011" after 1 ns, "0000000000000000" after 2 ns, "1010101010101010" after 3 ns,
+			"0000000011000000" after 4 ns, "0111111111111111" after 5 ns, "1001001001001001" after 6 ns,
+			"1111111111111111" after 7 ns, "1000000000000000" after 8 ns, "1000000000000000" after 9 ns,
+			"1001000100000011" after 11 ns, "0000000000000000" after 12 ns, "1010101010101010" after 13 ns,
+			"0000000011000000" after 14 ns, "0111111111111111" after 15 ns, "1001001001001001" after 16 ns,
+			"1111111111111111" after 17 ns, "1000000000000000" after 18 ns, "1000000000000000" after 19 ns,
+			"1001000100000011" after 21 ns, "0000000000000000" after 22 ns, "1010101010101010" after 23 ns,
+			"0000000011000000" after 24 ns, "0111111111111111" after 25 ns, "1001001001001001" after 26 ns,
+			"1111111111111111" after 27 ns, "1000000000000000" after 28 ns, "1000000000000000" after 29 ns;
+	B <= "0000000100000000" after 1 ns, "1111111111111111" after 2 ns, "0101010101010101" after 3 ns,
+			"0000000001000000" after 4 ns, "0000000000000001" after 5 ns, "0000000010000000" after 6 ns,
+			"0000000000000001" after 7 ns, "0000000000000001" after 8 ns, "1000000000000001" after 9 ns,
+			"0000000100000000" after 11 ns, "1111111111111111" after 12 ns, "0101010101010101" after 13 ns,
+			"0000000001000000" after 14 ns, "0000000000000001" after 15 ns, "0000000010000000" after 16 ns,
+			"0000000000000001" after 17 ns, "0000000000000001" after 18 ns, "1000000000000001" after 19 ns,
+			"0000000100000000" after 21 ns, "1111111111111111" after 22 ns, "0101010101010101" after 23 ns,
+			"0000000001000000" after 24 ns, "0000000000000001" after 25 ns, "0000000010000000" after 26 ns,
+			"0000000000000001" after 27 ns, "0000000000000001" after 28 ns, "1000000000000001" after 29 ns;
 
-end architecture generic_mux_10_utest;
+end architecture generic_alu_utest;
